f3a71519686e304a15b3797fcd0d9f091b5eca40
Levar Barrett <emaillevar@gmail.com>
2020-01-18 21:52:56 -0500

    routes

--- to-do-list/backend/server.js
+++ to-do-list/backend/server.js
@@ -3,7 +3,11 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const todoRoutes = express.Router();
 const PORT = 4000;
+
+//Bring in the model created in Schema;
+let Todo = require("./todo.model");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,6 +21,22 @@ const connection = mongoose.connection;
 connection.once("open", function() {
   console.log("MongoDB database connection established successfully");
 });
+
+//First endpoint of the router "/" mainpage.
+todoRoutes.route("/").get(function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+//Inserting Router and  Attaching the router URL path. Use "app.use"
+//because we are using middleware. Base route is "/todos"...all routes
+// are connected to this route
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, function() {
   console.log("Server is running on Port:" + PORT);
